411e033a2d75bb2c1cf8ccb705d882576b67c2c5
Jonathan Ballands <jballands@gmail.com>
2016-03-27 20:07:46 -0500

    Animations are pretty

---
+++ js/somersault/index.js
@@ -0,0 +1,10 @@
+/*
+ *  /js/somersault/index.js
+ *  © 2016 Jonathan Ballands
+ */
+
+import fitsInAnimation from './fitsInAnimation.js';
+//import fluidRoadmapAnimation from './fluidRoadmapAnimation.js';
+
+fitsInAnimation();
+//fluidRoadmapAnimation();

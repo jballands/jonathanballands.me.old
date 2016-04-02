9e2c277d2c693d588912ced11ff6135c4aff14da
Jonathan Ballands <jballands@gmail.com>
2016-04-01 22:32:54 -0500

    Navigation bar for mobile

---
+++ js/navigationBar/index.js
@@ -0,0 +1,27 @@
+/*
+ *  /js/navigationBar/index.js
+ *  © 2016 Jonathan Ballands
+ *
+ *  http://codepen.io/designcouch/pen/Atyop
+ *  http://codepen.io/g13nn/pen/eHGEF
+ */
+
+$(document).ready(() => {
+  let hamburger = $('#hamburger');
+  let hamburgerMenu = $('#hamburger-menu');
+  let hbmWork = $('#hbm-work');
+
+  hamburger.click(() => {
+    hamburger.toggleClass('open');
+    hamburgerMenu.toggleClass('open');
+  });
+
+  //
+  //  We need to handle the case where you click the work navigation bar item
+  //  and you are on the home page so the page won't technically reload
+  //
+  hbmWork.click(() => {
+    hamburger.toggleClass('open');
+    hamburgerMenu.toggleClass('open');
+  });
+});
